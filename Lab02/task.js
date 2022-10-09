94cfdfe0e589323d4b91e3e93676946393b8ca85
alafyte <nroussak0405@gmail.com>
2022-10-09 14:52:08 +0300

    Лабораторная работа №2

--- Lab02/task.js
+++ Lab02/task.js
@@ -1,52 +1,63 @@
-/*1. Напишите функцию, которая рассчитывает площадь, диаметр и длину окружности круга. Радиус круга вводит пользователь. */
+/*1. Напишите функции, которые рассчитывают площадь, диаметр и длину окружности круга. Радиус круга вводит пользователь. 
+Реализуйте функции как Function Declaration и Function Expression, функции-стрелки.*/
 let radius = Number(prompt("Введите радиус", ''));
-function circleProperties(radius) {
+
+//Площадь
+function circleAreaDec(radius) {
     alert(`Площадь круга радиусом ${radius} равна ${(Math.PI * Math.pow(radius, 2)).toFixed(2)}`);
+};
+
+let circleAreaEx = function(radius) {
+    alert(`Площадь круга радиусом ${radius} равна ${(Math.PI * Math.pow(radius, 2)).toFixed(2)}`);
+};
+
+let circleAreaArrow = (radius) => {
+    alert(`Площадь круга радиусом ${radius} равна ${(Math.PI * Math.pow(radius, 2)).toFixed(2)}`);
+};
+
+//Диаметр
+function circleDiameterDec(radius) {
     alert(`Диаметр круга радиусом ${radius} равен ${radius * 2}`);
+};
+
+let circleDiameterEx = function(radius) {
+    alert(`Диаметр круга радиусом ${radius} равен ${radius * 2}`);
+};
+
+let circleDiameterArrow = (radius) => {
+    alert(`Диаметр круга радиусом ${radius} равен ${radius * 2}`);
+};
+
+//Длина окружности
+function circumferenceDec(radius) {
     alert(`Длина окружности круга радиусом ${radius} равна ${(2 * Math.PI * radius).toFixed(2)}`);
-}
-circleProperties(radius);
+};
 
-/*2. Реализуйте функцию, которая сообщает пользователю, что его заказ на сумму s принят. Пользователь добавляет товары в корзину 
-(вводит стоимость каждого товара в окно prompt). Если сумма стоимости товаров превысила денежные средства на банковской карте покупателя, 
-сообщите об этом пользователю и предупредите, что последний товар он купить не может, поэтому он должен либо завершить покупку, либо выбрать 
-товар с меньшей стоимостью, которая не превышает остаток n на карте. Чтобы завершить покупку пользователь должен ввести «0». */
-function order(balance) {
-    while (true) {
-        let priceOfProduct = Number(prompt("Введите стоимость товара: ", ''));
-        balance -= priceOfProduct;
+let circumferenceEx = function(radius) {
+    alert(`Длина окружности круга радиусом ${radius} равна ${(2 * Math.PI * radius).toFixed(2)}`);
+};
 
-        if (balance < 0) {
-            balance += priceOfProduct;
-            alert("Сумма товаров превышает денежные средства на вашей банковской карте." +
-            "Выберите товар с меньшей стоимостью или завершите покупку. Баланс: " + balance);
-            let exit = Number(prompt("Введите 0 для завершения покупки или стоимость другого товара: "), '');
+let circumferenceArrow = (radius) => {
+    alert(`Длина окружности круга радиусом ${radius} равна ${(2 * Math.PI * radius).toFixed(2)}`);
+};
 
-            if (exit == 0) {
-                break;
-            }
-        }
-    }
-}
 
-order(200);
-
-/*3. Реализуйте функцию с тремя параметрами. Первый параметр по умолчанию. 
+/*2. Реализуйте функцию с тремя параметрами. Первый параметр по умолчанию. 
 Третий параметр вводит пользователь. Функция возвращает строку из трех параметров. */
 let thirdParameter = prompt("Введите третий параметр", '');
 
-function threeParameters(secondParameter, thirdParameter, firstParameter = "L") {
+function threeParameters(secondParameter, thirdParameter, firstParameter = "JavaScript") {
     alert(firstParameter + secondParameter + thirdParameter);
 }
-threeParameters("Hi", thirdParameter);
+threeParameters(" is a ", thirdParameter);
 
-/*4. Реализуйте функцию, которая рассчитывает количество присутствующих студентов. 
+/*3. Реализуйте функцию, которая рассчитывает количество присутствующих студентов. 
 Имена присутствующих студентов пользователь вводит в модальное окно. */
 function counterOfStudents() {
     let counter = 0
     while (true) {
-        let student = prompt("Введите имя студента (Чтобы прекратить ввод, введите 0): ", '');
-        if (student == 0) {
+        let student = prompt("Введите имя студента (Чтобы прекратить ввод, введите 'стоп'): ", '');
+        if (student.toLowerCase() == "стоп") {
             return counter;
         }
         counter++
@@ -55,30 +66,7 @@ function counterOfStudents() {
 let countOfStudents = counterOfStudents();
 alert(countOfStudents);
 
-/*5. Известны стороны четырехугольника a и b. Если это квадрат, то функция params() возвращает его периметр, 
-если прямоугольник –  то площадь. Создайте params() как Function Declaration и Function Expression. 
-Вызовите ее при создании («на месте»).*/ 
-function paramsVer1 (firstSide, secondSide) {
-    if (firstSide == secondSide) {
-        alert(firstSide * 4);
-    }
-    else {
-        alert(firstSide * secondSide);
-    }
-};
-paramsVer1(7, 7);
-
-let paramsVer2 = function (firstSide, secondSide) {
-    if (firstSide == secondSide) {
-        return firstSide * 4;
-    }
-    else {
-        return firstSide * secondSide;
-    }
-}(5, 6);
-alert(paramsVer2); //30
-
-/*6. Вы забыли пароль от электронной почты. Вы помните, что он состоит из 8 символов нижнего регистра. 
+/*4. Вы забыли пароль от электронной почты. Вы помните, что он состоит из 8 символов нижнего регистра. 
 Первые 5 – это буквы английского алфавита, последние 3 – цифры.  Один пароль вы успеваете ввести за 3 секунды. 
 Сколько максимум времени вам понадобится, чтобы подобрать пароль? Выведите результат в виде строки «y лет m месяцев d дней h часов 
 min минут s секунд». Для простоты решения можно принять, что в месяце 30 дней. */
@@ -119,10 +107,74 @@ function passwordGuessingTime() {
 }
 passwordGuessingTime();
 
-/*Задачу 3 решите, используя функцию-стрелку. */
-let threeParametersVer2  = (secondParameter, thirdParameter, firstParameter = "L") => 
-    alert(firstParameter + secondParameter + thirdParameter);
+/*5. Пользователь вводит данные. Если он ввел число, то преобразуйте его в 16-ричную систему счисления (вывод в верхнем регистре). 
+Если число дробное – округлите его до наибольшего, наименьшего и ближайшего целого. Если пользователь ввел текст, то преобразуйте его к 
+верхнему и нижнему регистру. */
 
+while (true) {
+    let data = prompt("Вводите данные. Чтобы прекратить ввод, введите 'стоп'");
+    if (data.toLowerCase() == "стоп") {
+        break;
+    } else if (Number(data)) { 
+        if (Number.isInteger(Number(data))) {
+            alert(Number(data).toString(16).toUpperCase());
+        } else {
+            alert(Math.ceil(Number(data)));
+            alert(Math.floor(Number(data)));
+            alert(Math.round(Number(data)));
+        }
 
-threeParametersVer2("World", thirdParameter, "Hello!");
+    } else {
+        alert(data.toLowerCase());
+        alert(data.toUpperCase());
+    }
+}
 
+/*6. Выпускник сдает ЦТ по русскому языку. Ему дано словарное слово, необходимо ввести в текстовое поле правильный вариант ответа. 
+Проверьте его ответ и сообщите в каком символе он допустил ошибку, если она есть. */
+const dictionaryWord = "Абажур";
+
+let studentsAnswer = prompt("Введите словарное слово", '');
+let wrongSymbol;
+
+if (studentsAnswer.toLowerCase() != "абажур") {
+    for (i = 0; i < dictionaryWord.length - 1; i++) {
+        if (dictionaryWord[i].toLowerCase() != studentsAnswer[i].toLowerCase()) {
+            wrongSymbol = i;
+            break;
+        }
+    }
+    alert(`Ошибка! Неверный символ: ${studentsAnswer[wrongSymbol]}, позиция: ${wrongSymbol + 1}`);
+} else {
+    alert("Всё верно!");
+}
+
+/*7. Разработайте геометрический калькулятор для расчета параметров треугольника: площадь, периметр, высота, cos, sin, tg, ctg. 
+Пользователь указывает длину катетов. */
+let firstLeg = Number(prompt("Введите длину первого катета", ''));
+let secondLeg = Number(prompt("Введите длину второго катета", ''));
+let hypotenuse = Math.sqrt(Math.pow(firstLeg, 2) + Math.pow(secondLeg, 2)).toFixed(2);
+
+let triangleArea = 0.5 * firstLeg * secondLeg;
+let trianglePerimeter = firstLeg + secondLeg + hypotenuse;
+let triangleHeight = firstLeg * secondLeg / hypotenuse;
+let cos1 = firstLeg / hypotenuse;
+let cos2 = secondLeg / hypotenuse;
+let sin1 = secondLeg / hypotenuse;
+let sin2 = firstLeg / hypotenuse;
+let tg1 = secondLeg / firstLeg;
+let tg2 = firstLeg / secondLeg;
+let ctg1 = firstLeg / secondLeg;
+let ctg2 = secondLeg / firstLeg;
+
+alert(`Площадь треугольника: ${triangleArea}`);
+alert(`Площадь треугольника: ${trianglePerimeter}`);
+alert(`Высота треугольника: ${triangleHeight}`);
+alert(`Косинус первого угла треугольника: ${cos1}`);
+alert(`Косинус второго угла треугольника: ${cos2}`);
+alert(`Синус первого угла треугольника: ${sin1}`);
+alert(`Синус второго угла треугольника: ${sin2}`);
+alert(`Тангенс первого угла треугольника: ${tg1}`);
+alert(`Тангенс второго угла треугольника: ${tg2}`);
+alert(`Котангенс первого угла треугольника: ${ctg1}`);
+alert(`Котангенс второго угла треугольника: ${ctg2}`);
